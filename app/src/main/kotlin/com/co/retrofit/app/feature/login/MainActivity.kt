ba4bf35511a97f5dadd772c330063e4bbbe3486a
Xiomara <xiomaracortes0@gmail.com>
2021-10-21 19:28:55 -0500

    nUpdate the code

--- app/src/main/kotlin/com/co/retrofit/app/feature/login/MainActivity.kt
+++ app/src/main/kotlin/com/co/retrofit/app/feature/login/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottomNavigationView = findViewById(R.id.nav_view)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
         val navController = findNavController(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController(navController)
     }
