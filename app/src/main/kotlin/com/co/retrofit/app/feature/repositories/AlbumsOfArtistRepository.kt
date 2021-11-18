7e006d7b542a2d7b6577e77642c35887d8d17258
Angelica <am.rodriguez12@uniandes.edu.co>
2021-11-18 11:22:53 -0500

    Se implementa estructura de caché con HashMap

--- app/src/main/kotlin/com/co/retrofit/app/feature/repositories/AlbumsOfArtistRepository.kt
+++ app/src/main/kotlin/com/co/retrofit/app/feature/repositories/AlbumsOfArtistRepository.kt
@@ -1,13 +1,28 @@
 package com.co.retrofit.app.feature.repositories
 
 import android.app.Application
+import android.util.Log
 import com.android.volley.VolleyError
 import com.co.retrofit.app.feature.model.dto.Album
+import com.co.retrofit.app.feature.network.CacheManager
 import com.co.retrofit.app.feature.network.NetworkServiceAdapter
 
 class AlbumsOfArtistRepository (val application: Application) {
+
     suspend fun refreshData(artistId: Int):List<Album> {
-        return NetworkServiceAdapter.getInstance(application).getAlbumsOfArtist(artistId)
+        var potentialResp = CacheManager.getInstance(application.applicationContext).getAlbumsOfArtist(artistId)
+        if(potentialResp.isEmpty()){
+            Log.d("Cache decision", "get from network")
+            var albumsOfArtist = NetworkServiceAdapter.getInstance(application).getAlbumsOfArtist(artistId)
+            CacheManager.getInstance(application.applicationContext).addAlbumsOfArtist(artistId, albumsOfArtist)
+            return albumsOfArtist
+        }
+        else{
+            Log.d("Cache decision", "return ${potentialResp.size} elements from cache")
+            return potentialResp
+        }
+
+
     }
 
 
