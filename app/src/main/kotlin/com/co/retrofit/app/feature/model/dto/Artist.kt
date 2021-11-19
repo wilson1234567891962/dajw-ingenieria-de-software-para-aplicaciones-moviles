8bd2ae2718c09bc682b65a1d49dea1d346a0fbc5
Angelica <am.rodriguez12@uniandes.edu.co>
2021-11-18 20:04:45 -0500

    Se inicia implementacion de room

--- app/src/main/kotlin/com/co/retrofit/app/feature/model/dto/Artist.kt
+++ app/src/main/kotlin/com/co/retrofit/app/feature/model/dto/Artist.kt
@@ -1,10 +1,14 @@
 package com.co.retrofit.app.feature.model.dto
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity(tableName = "albums_table")
 data class Artist(
+    @PrimaryKey val albumId:Int,
     val artistId:Int,
     val name:String,
     val image:String,
