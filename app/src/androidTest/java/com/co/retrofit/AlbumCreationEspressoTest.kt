9b7a5b253d9f56377e59656ded960cb29f0ffd3c
Xiomara <xiomaracortes0@gmail.com>
2021-11-22 12:45:03 -0500

    Create the test

--- app/src/androidTest/java/com/co/retrofit/AlbumCreationEspressoTest.kt
+++ app/src/androidTest/java/com/co/retrofit/AlbumCreationEspressoTest.kt
@@ -1,13 +1,9 @@
 package com.co.retrofit
 
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
 import com.co.retrofit.app.R
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.swipeUp
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
@@ -19,16 +15,8 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.espresso.action.ViewActions.typeText
-
-import androidx.test.espresso.action.ViewActions.clearText
-
-import androidx.test.espresso.matcher.ViewMatchers.withClassName
-
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.ViewMatchers
 import org.hamcrest.Matchers.allOf
-
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -59,6 +47,7 @@ class AlbumCreationEspressoTest {
             .perform(typeText("23790125016"))
         onView(withId(R.id.caratula_text))
             .check(matches(hasValueEqualTo("23790125016")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
     }
 
 
@@ -74,9 +63,79 @@ class AlbumCreationEspressoTest {
             .perform(typeText("23790125016"))
         onView(withId(R.id.caratula_text))
             .check(matches(hasValueEqualTo("23790125016")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
         onView(allOf(withId(R.id.name_input), isDisplayed()))
             .perform(typeText("Yerli Xiomara Cortez Espalza"))
         onView(allOf(withId(R.id.name_input), isDisplayed()))
             .perform(typeText("Yerli Xiomara Cortez Espalza"))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
     }
+
+    @Test
+    fun wheTheUserOpensTheApp_HeGoesTheAlbumCreate_HeFillOutTheRelease() {
+        onView(withId(R.id.rv_album_list))
+            .check(matches(atPosition(0, hasDescendant(withText("Género: Salsa")))))
+        onView(withId(R.id.fab))
+            .perform(click())
+        onView(withId(R.id.arrow_title))
+            .check(matches(hasValueEqualTo("Añadir album")))
+        onView(allOf(withId(R.id.caratula_text), isDisplayed()))
+            .perform(typeText("23790125016"))
+        onView(withId(R.id.caratula_text))
+            .check(matches(hasValueEqualTo("23790125016")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(allOf(withId(R.id.name_input), isDisplayed()))
+            .perform(typeText("Yerli Xiomara Cortez Espalza"))
+        onView(withId(R.id.name_input))
+            .check(matches(hasValueEqualTo("Yerli Xiomara Cortez Espalza")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(allOf(withId(R.id.artista_input), isDisplayed()))
+            .perform(typeText("Test"))
+        onView(withId(R.id.artista_input))
+            .check(matches(hasValueEqualTo("Test")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(allOf(withId(R.id.lanzamiento_input), isDisplayed()))
+            .perform(typeText("1999-06-13"))
+        onView(withId(R.id.lanzamiento_input))
+            .check(matches(hasValueEqualTo("1999-06-13")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+    }
+
+
+    @Test
+    fun wheTheUserOpensTheApp_HeGoesTheAlbumCreate_HeFillOutTheGender() {
+        onView(withId(R.id.rv_album_list))
+            .check(matches(atPosition(0, hasDescendant(withText("Género: Salsa")))))
+        onView(withId(R.id.fab))
+            .perform(click())
+        onView(withId(R.id.arrow_title))
+            .check(matches(hasValueEqualTo("Añadir album")))
+        onView(allOf(withId(R.id.caratula_text), isDisplayed()))
+            .perform(typeText("23790125016"))
+        onView(withId(R.id.caratula_text))
+            .check(matches(hasValueEqualTo("23790125016")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(allOf(withId(R.id.name_input), isDisplayed()))
+            .perform(typeText("Yerli Xiomara Cortez Espalza"))
+        onView(withId(R.id.name_input))
+            .check(matches(hasValueEqualTo("Yerli Xiomara Cortez Espalza")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(allOf(withId(R.id.artista_input), isDisplayed()))
+            .perform(typeText("Test"))
+        onView(withId(R.id.artista_input))
+            .check(matches(hasValueEqualTo("Test")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(allOf(withId(R.id.lanzamiento_input), isDisplayed()))
+            .perform(typeText("1999-06-13"))
+        onView(withId(R.id.lanzamiento_input))
+            .check(matches(hasValueEqualTo("1999-06-13")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(allOf(withId(R.id.Genero_input), isDisplayed()))
+            .perform(typeText("Salsa"))
+        onView(withId(R.id.Genero_input))
+            .check(matches(hasValueEqualTo("Salsa")))
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard())
+    }
+
+  
 }
